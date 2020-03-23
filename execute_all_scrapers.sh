982c966d3075c6469d8a2b56cb8b68ea23cfe235
Hoonst <yoonhs1993@nate.com>
2020-03-23 03:56:14 +0000

    pyenv setting and make it run

--- execute_all_scrapers.sh
+++ execute_all_scrapers.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /home/yoonhoonsang/internet_lecture
-source scraper/bin/activate
 cd qna_crawler
 
 scrapy crawl SPIDERMAN -a with_range=False -a teacher=etoos_yhk
@@ -12,6 +11,11 @@ scrapy crawl SPIDERMAN -a with_range=False -a teacher=megastudy_kkh
 scrapy crawl SPIDERMAN -a with_range=False -a teacher=megastudy_jjs
 scrapy crawl SPIDERMAN -a with_range=False -a teacher=megastudy_kkc
 scrapy crawl SPIDERMAN -a with_range=False -a teacher=megastudy_jjh
+
+scrapy crawl SPIDERMAN -a with_range=False -a teacher=mimac_lmh
+scrapy crawl SPIDERMAN -a with_range=False -a teacher=mimac_lys
+scrapy crawl SPIDERMAN -a with_range=False -a teacher=mimac_esj
+scrapy crawl SPIDERMAN -a with_range=False -a teacher=mimac_kjj
 
 scrapy crawl SPIDERMAN -a with_range=False -a teacher=skyedu_jhc
 scrapy crawl SPIDERMAN -a with_range=False -a teacher=skyedu_jej
--- qna_crawler/qna_crawler/spiders/lecture_scraper.py
+++ qna_crawler/qna_crawler/spiders/lecture_scraper.py
@@ -24,6 +24,7 @@ from selenium.webdriver.firefox.options import Options
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
+
 def get_proxies(co):
     driver = webdriver.Chrome(chrome_options=co)
     driver.get("https://free-proxy-list.net/")
@@ -39,13 +40,14 @@ def get_proxies(co):
     return PROXIES
 
 
-#ALL_PROXIES = get_proxies(co)
+# ALL_PROXIES = get_proxies(co)
+
 
 def proxy_driver(PROXIES, co):
     prox = Proxy()
     pxy = ""
     if PROXIES:
-        pxy = random.choice(PROXIES) #random proxy
+        pxy = random.choice(PROXIES)  # random proxy
         print(pxy)
     else:
         print("--- Proxies used up (%s)" % len(PROXIES))
@@ -62,11 +64,12 @@ def proxy_driver(PROXIES, co):
 
     return driver
 
+
 def proxy_driver_with_firefox(PROXIES, co):
     prox = Proxy()
     pxy = ""
     if PROXIES:
-        pxy = random.choice(PROXIES) #random proxy
+        pxy = random.choice(PROXIES)  # random proxy
         print(pxy)
     else:
         print("--- Proxies used up (%s)" % len(PROXIES))
@@ -79,31 +82,39 @@ def proxy_driver_with_firefox(PROXIES, co):
     capabilities = DesiredCapabilities.FIREFOX
     prox.add_to_capabilities(capabilities)
 
-    driver = webdriver.Firefox(firefox_options=co, desired_capabilities=capabilities, executable_path="/usr/local/bin/geckodriver")
+    driver = webdriver.Firefox(
+        firefox_options=co,
+        desired_capabilities=capabilities,
+        executable_path="/usr/local/bin/geckodriver",
+    )
 
     return driver
 
 
+##########################################################################################################
+##########################################################################################################
+##########################################################################################################
 
-##########################################################################################################
-##########################################################################################################
-##########################################################################################################
 
 def parse_date(date_value):
     return parse(date_value)
+
 
 def set_range(start, till):
     start, till = parse(start).date(), parse(till).date()
     return start, till
 
+
 def set_range_with_today():
     today = datetime.date.today()
-    yesterday = (today - datetime.timedelta(days=1))
-    #day_before_yesterday = yesterday - datetime.timedelta(days=1)
+    yesterday = today - datetime.timedelta(days=1)
+    # day_before_yesterday = yesterday - datetime.timedelta(days=1)
     return yesterday, yesterday
+
 
 def set_teachers(teacher_name, doc):
     return doc.worksheet(teacher_name)
+
 
 def mergeDict(dict1, dict2):
     """ Merge dictionaries and keep values of common keys in list"""
@@ -114,6 +125,7 @@ def mergeDict(dict1, dict2):
 
     return dict3
 
+
 def check_date_range(date, start, till):
     if date <= start and date >= till:
         return 1
@@ -121,6 +133,7 @@ def check_date_range(date, start, till):
         return 0
     elif date < till:
         return -1
+
 
 def row_filter(rows, teacher_name, start, till):
     run = True
@@ -142,7 +155,7 @@ def row_filter(rows, teacher_name, start, till):
                 continue
             print("This is a question")
             check = check_date_range(date_value, start, till)
-            print('check: ', check)
+            print("check: ", check)
             if check > 0:
                 qna_dic[date_value] += 1
             elif check == 0:
@@ -151,10 +164,10 @@ def row_filter(rows, teacher_name, start, till):
             elif check < 0:
                 print("out of date-range, less {}".format(str(till)))
                 run = False
-                print('I am broken')
+                print("I am broken")
                 break
 
-            print('====================')
+            print("====================")
         return run, qna_dic
 
     # SKYEDU
@@ -172,7 +185,7 @@ def row_filter(rows, teacher_name, start, till):
             elif check < 0:
                 print("out of date-range, less {}".format(str(till)))
                 run = False
-                print('I am broken')
+                print("I am broken")
                 break
         return run, qna_dic
 
@@ -192,7 +205,7 @@ def row_filter(rows, teacher_name, start, till):
             elif check < 0:
                 print("out of date-range, less {}".format(str(till)))
                 run = False
-                print('I am broken')
+                print("I am broken")
                 break
 
         return run, qna_dic
@@ -217,39 +230,37 @@ def row_filter(rows, teacher_name, start, till):
             elif check < 0:
                 print("out of date-range, less {}".format(str(till)))
                 run = False
-                print('I am broken')
+                print("I am broken")
                 break
 
         return run, qna_dic
 
+
 def wait_element(browser, teacher_name):
-    if 'etoos' in teacher_name:
+    if "etoos" in teacher_name:
         title = WebDriverWait(browser, 10).until(
-                EC.presence_of_element_located(
-                    (By.CSS_SELECTOR, "table.subcomm_tbl_board")
+            EC.presence_of_element_located((By.CSS_SELECTOR, "table.subcomm_tbl_board"))
+        )
+    elif "megastudy" in teacher_name:
+        title = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located(
+                (
+                    By.CSS_SELECTOR,
+                    "div.table_list > table.commonBoardList > tbody > tr.top",
                 )
             )
-    elif 'megastudy' in teacher_name:
+        )
+    elif "skyedu" in teacher_name:
         title = WebDriverWait(browser, 10).until(
-                    EC.presence_of_element_located(
-                        (
-                            By.CSS_SELECTOR,
-                            "div.table_list > table.commonBoardList > tbody > tr.top",
-                        )
-                    )
-                )
-    elif 'skyedu' in teacher_name:
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div.board-list > table"))
+        )
+    elif "mimac" in teacher_name:
         title = WebDriverWait(browser, 10).until(
-                    EC.presence_of_element_located(
-                        (By.CSS_SELECTOR, "div.board-list > table")
-                    )
-                )
-    elif 'mimac' in teacher_name:
-        title = WebDriverWait(browser, 10).until(
-                    EC.presence_of_element_located(
-                        (By.CSS_SELECTOR, "div.tbltype_list > table")
-                    )
-                )
+            EC.presence_of_element_located(
+                (By.CSS_SELECTOR, "div.tbltype_list > table")
+            )
+        )
+
 
 def connect_gspread():
     scope = [
@@ -273,18 +284,20 @@ def connect_gspread():
 def driver_setting():
     ua = UserAgent()
     co = webdriver.ChromeOptions()
-    co.add_argument("/home/yoonhoonsang/internet_lecture/chromedriver")
+    co.add_argument('/home/yoonhoonsang/internet_lecture/chromedriver')
     co.add_argument("log-level=1")
     co.add_argument("headless")
     co.add_argument("user-agent={}".format(ua.random))
     co.add_argument("lang=ko_KR")
     return co
 
+
 def driver_setting_firefox():
     opts = Options()
-    #opts.add_argument('--headless')
-    print('Firefox Headless Browser Invoked')
+    # opts.add_argument('--headless')
+    print("Firefox Headless Browser Invoked")
     return opts
+
 
 def find_rows_of_table(browser):
     tbody = browser.find_element_by_tag_name("tbody")
@@ -301,39 +314,36 @@ class SPIDER_BOARD(scrapy.Spider):
         # Arguments:
         # self.start, self.till, self.teacher
         # if only self.teacher:   set_range_with_today()
-        if self.with_range == 'False':
+        if self.with_range == "False":
             self.start, self.till = set_range_with_today()
 
-        elif self.with_range == 'True':
+        elif self.with_range == "True":
             self.start, self.till = set_range(self.start, self.till)
 
         self.co = driver_setting()
-        #self.firefox_setting = driver_setting_firefox()
+        # self.firefox_setting = driver_setting_firefox()
         self.doc = connect_gspread()
         self.worksheet = set_teachers(self.teacher, self.doc)
         self.teacher_dic = {
-            #ETOOS
+            # ETOOS
             "etoos_yhk": "https://www.etoos.com/teacher/board/sub04_math/board_list.asp?teacher_id=200386&selSearchType=&txtSearchWD=&BOARD_ID=2007&QUST_TYPE_CD=&GOOD_QUST_YN=&MOV_YN=&MEM_YN=&NTView=&page={}",
             "etoos_kww": "https://www.etoos.com/teacher/board/sub04_math/board_list.asp?teacher_id=200245&selSearchType=&txtSearchWD=&BOARD_ID=2007&QUST_TYPE_CD=&GOOD_QUST_YN=&MOV_YN=&MEM_YN=&NTView=&page={}",
             "etoos_swc": "https://www.etoos.com/teacher/board/sub04_math/board_list.asp?teacher_id=200236&selSearchType=&txtSearchWD=&BOARD_ID=2007&QUST_TYPE_CD=&GOOD_QUST_YN=&MOV_YN=&MEM_YN=&NTView=&page={}",
             "etoos_grace": "https://www.etoos.com/teacher/board/sub04_math/board_list.asp?teacher_id=200331&selSearchType=&txtSearchWD=&BOARD_ID=2007&QUST_TYPE_CD=&GOOD_QUST_YN=&MOV_YN=&MEM_YN=&NTView=&page={}",
-
-            #MEGASTUDY
+            # MEGASTUDY
             "megastudy_jjs": "http://www.megastudy.net/teacher_v2/bbs/bbs_list_ax.asp?tec_cd=rimbaud666&tec_nm=%uC870%uC815%uC2DD&tec_type=1&brd_cd=784&brd_tbl=MS_BRD_TEC784&brd_kbn=qnabbs&dom_cd=5&LeftMenuCd=3&LeftSubCd=1&HomeCd=134&page={}&chr_cd=&sub_nm=&ans_yn=&smode=1&sword=&TmpFlg=0.24915805251066403",
             "megastudy_kkh": "http://www.megastudy.net/teacher_v2/bbs/bbs_list_ax.asp?tec_cd=megakkh&tec_nm=%uAE40%uAE30%uD6C8&tec_type=1&brd_cd=28&brd_tbl=MS_BRD_TEC28&brd_kbn=qnabbs&dom_cd=5&LeftMenuCd=3&LeftSubCd=1&HomeCd=62&page={}&chr_cd=&sub_nm=&ans_yn=&smode=1&sword=&TmpFlg=0.06499842812264811",
             "megastudy_kkc": "http://www.megastudy.net/teacher_v2/bbs/bbs_list_ax.asp?tec_cd=kichery&tec_nm=%uAE40%uAE30%uCCA0&tec_type=1&brd_cd=802&brd_tbl=MS_BRD_TEC802&brd_kbn=qnabbs&dom_cd=5&LeftMenuCd=3&LeftSubCd=1&HomeCd=145&page={}&chr_cd=&sub_nm=&ans_yn=&smode=1&sword=&TmpFlg=0.9052209945738199",
             "megastudy_jjh": "http://www.megastudy.net/teacher_v2/bbs/bbs_list_ax.asp?tec_cd=megachrisjo21&tec_nm=%uC870%uC815%uD638&tec_type=1&brd_cd=531&brd_tbl=MS_BRD_TEC531&brd_kbn=qnabbs&dom_cd=5&LeftMenuCd=3&LeftSubCd=1&HomeCd=73&page={}&chr_cd=&sub_nm=&ans_yn=&smode=1&sword=&TmpFlg=0.19749594326445652",
-
-            #SKYEDU
+            # SKYEDU
             "skyedu_jej": "https://skyedu.conects.com/teachers/teacher_qna/?t_id=jej01&cat1=1&page={}",
             "skyedu_jhc": "https://skyedu.conects.com/teachers/teacher_qna/?t_id=jhc01&cat1=1&page={}",
-
-            #MIMAC
+            # MIMAC
             "mimac_lmh": "http://www.mimacstudy.com/tcher/studyQna/getStudyQnaList.ds?tcd=531&currPage={}&myQna=N&ordType=&pageType=home&srchWordType=title&relm=03&type=03531&tcdTabType=tcdHome&menuIdx=&relmName=&tcdName=",
             "mimac_lys": "http://www.mimacstudy.com/tcher/studyQna/getStudyQnaList.ds?tcd=926&currPage={}&myQna=N&ordType=&pageType=home&srchWordType=title&relm=03&type=03926&tcdTabType=tcdHome&menuIdx=&relmName=&tcdName=",
             "mimac_esj": "http://www.mimacstudy.com/tcher/studyQna/getStudyQnaList.ds?tcd=503&currPage={}&myQna=N&ordType=&pageType=home&srchWordType=title&relm=03&type=03503&tcdTabType=tcdHome&menuIdx=&relmName=&tcdName=",
             "mimac_kjj": "http://www.mimacstudy.com/tcher/studyQna/getStudyQnaList.ds?tcd=536&currPage={}&myQna=N&ordType=&pageType=home&srchWordType=title&relm=03&type=03536&tcdTabType=tcdHome&menuIdx=&relmName=&tcdName=",
-            "mimac_hjo": "http://www.mimacstudy.com/tcher/studyQna/getStudyQnaList.ds?tcd=922&currPage={}&myQna=N&ordType=&pageType=home&srchWordType=title&relm=03&type=03922&tcdTabType=tcdHome&menuIdx=&relmName=&tcdName="
+            "mimac_hjo": "http://www.mimacstudy.com/tcher/studyQna/getStudyQnaList.ds?tcd=922&currPage={}&myQna=N&ordType=&pageType=home&srchWordType=title&relm=03&type=03922&tcdTabType=tcdHome&menuIdx=&relmName=&tcdName=",
         }
 
     def parse(self, response):
@@ -344,10 +354,10 @@ class SPIDER_BOARD(scrapy.Spider):
         page, qna_dictionary, running = 1, defaultdict(int), True
         print(driver_setting())
         while running:
-#             if 'mimac' in self.teacher:
-#                 browser = proxy_driver_with_firefox(ALL_PROXIES, self.firefox_setting)
-#             else:
-            browser = webdriver.Chrome(chrome_options=self.co)
+            #             if 'mimac' in self.teacher:
+            #                 browser = proxy_driver_with_firefox(ALL_PROXIES, self.firefox_setting)
+            #             else:
+            browser = webdriver.Chrome(chrome_options=self.co, executable_path='/home/yoonhoonsang/internet_lecture/chromedriver')
             print("Current Page {}".format(page))
             base_url = self.teacher_dic[self.teacher].format(str(page))
             browser.get(base_url)
@@ -373,5 +383,3 @@ class SPIDER_BOARD(scrapy.Spider):
             item["qna_count"] = qna_dictionary[date]
 
             yield item
-
-
